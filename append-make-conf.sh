4e00d3ea201137630e68b4b521e6a7e39e032058
hanyuu <hanyuujp5192@protonmail.com>
2024-11-12 23:56:20 +0000

    added chroot warning

--- append-make-conf.sh
+++ append-make-conf.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+# Ensure you are chrooted into your environment before running this!
+
 emerge --info | grep ^USE >> /etc/portage/make.conf
 sed -i -e                    's/^COMMON_FLAGS=.*$/COMMON_FLAGS="-march=native -O2 -pipe"/' /etc/portage/make.conf
 echo 'MAKEOPTS="-j10"'    >> /etc/portage/make.conf
