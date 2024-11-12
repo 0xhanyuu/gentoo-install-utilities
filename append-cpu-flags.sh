575c1ee5aacaae325fb62ad33f5166a7030696c9
hanyuu <hanyuujp5192@protonmail.com>
2024-11-12 23:55:57 +0000

    added chroot warning

--- append-cpu-flags.sh
+++ append-cpu-flags.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
 
+# Ensure you are chrooted into your environment before running this!
+
 emerge --ask --oneshot app-portage/cpuid2cpuflags
 echo "*/* $(cpuid2cpuflags)" > /etc/portage/package.use/00cpu-flags
