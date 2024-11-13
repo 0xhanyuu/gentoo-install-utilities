fba8cf06ad819ce4fd591677cc010f67bf0a7059
hanyuu <hanyuujp5192@protonmail.com>
2024-11-13 00:32:51 +0000

    replaced dysfunctional interactive prompt

--- mount-pseudo-fs.sh
+++ mount-pseudo-fs.sh
@@ -1,34 +1,15 @@
 #!/bin/bash
 
-input="y"
-location=""
+# Feel free to change this directory if needed.
+location='/mnt/gentoo'
 
-echo "Is your mounted filesystem located at '/mnt/gentoo'?"
-read -p "> " input
-
-if [[ input == 'y' ]]; then
-    mount --types proc /proc /mnt/gentoo/proc
+mount --types proc /proc $location/proc -v
     
-    mount --rbind /sys       /mnt/gentoo/sys
-    mount --make-rslave      /mnt/gentoo/sys
+mount --rbind /sys       $location/sys -v
+mount --make-rslave      $location/sys -v
     
-    mount --rbind /dev       /mnt/gentoo/dev
-    mount --make-rslave      /mnt/gentoo/dev
+mount --rbind /dev       $location/dev -v
+mount --make-rslave      $location/dev -v
     
-    mount --bind /run        /mnt/gentoo/run
-    mount --make-slave       /mnt/gentoo/run
-else
-    echo "Please provide your mount location:"
-    read -p "> " location
-
-    mount --types proc /proc $location/proc
-    
-    mount --rbind /sys       $location/sys
-    mount --make-rslave      $location/sys
-    
-    mount --rbind /dev       $location/dev
-    mount --make-rslave      $location/dev
-    
-    mount --bind /run        $location/run
-    mount --make-slave       $location/run
-fi
+mount --bind /run        $location/run -v
+mount --make-slave       $location/run -v
